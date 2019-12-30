bd061f993baeb61a3a6bcbc561414f3a45b93969
1450032191 <1450032191@qq.com>
2019-12-30 18:46:11 +0800

    dao包提交

---
+++ src/cn/edu/xmut/lgrg/dao/SysUserDao.java
@@ -0,0 +1,9 @@
+package cn.edu.xmut.lgrg.dao;
+
+/**
+ * @Auther: ZiNan
+ * @Date: 2019/12/30 18:45
+ * @Description:
+ */
+public interface SysUserDao {
+}
---
+++ src/cn/edu/xmut/lgrg/dao/impl/SysUserImpl.java
@@ -0,0 +1,9 @@
+package cn.edu.xmut.lgrg.dao.impl;
+
+/**
+ * @Auther: ZiNan
+ * @Date: 2019/12/30 18:45
+ * @Description:
+ */
+public class SysUserImpl {
+}
