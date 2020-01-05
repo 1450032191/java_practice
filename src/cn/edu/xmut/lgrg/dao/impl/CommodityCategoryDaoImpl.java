1d3de879adf9fff8671bcbb33a759710f6651eaf
Cakoen <601814946cjk@gmail.com>
2020-01-05 21:18:55 +0800

    商品详情，分类详情代码更新

--- src/cn/edu/xmut/lgrg/dao/impl/CommodityCategoryDaoImpl.java
+++ src/cn/edu/xmut/lgrg/dao/impl/CommodityCategoryDaoImpl.java
@@ -2,8 +2,14 @@ package cn.edu.xmut.lgrg.dao.impl;
 
 import cn.edu.xmut.lgrg.dao.CommodityCategoryDao;
 import cn.edu.xmut.lgrg.entity.CommodityCategory;
+import cn.edu.xmut.lgrg.entity.PageData;
+import cn.edu.xmut.lgrg.entity.SysUser;
 import cn.edu.xmut.lgrg.util.ConnDBUtil;
+import cn.edu.xmut.lgrg.util.MySqlUtil;
+import cn.edu.xmut.lgrg.util.ResultSetUtil;
+import cn.edu.xmut.lgrg.util.StringUtil;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -13,45 +19,37 @@ public class CommodityCategoryDaoImpl implements CommodityCategoryDao {
     @Override
     public void addCategory(CommodityCategory category) throws Exception {
         String sql = "INSERT INTO commodity_category(`name`,person_id,`status`,create_time) VALUES(?,?,?,?)";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
-        connDB.OpenConn();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, category.getName());
             pstm.setString(2, category.getPersonId());
             pstm.setString(3, category.getStatus());
             pstm.setString(4, category.getCreateTime());
             pstm.executeUpdate();
-            pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("增加数据错误！");
-        } finally {
-            connDB.closeConn();
         }
     }
 
     @Override
     public void deleteCategory(CommodityCategory category) throws Exception {
         String sql = "delete from commodity_category where id=?";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
-        connDB.OpenConn();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, category.getId());
             pstm.executeUpdate();
-            pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("删除错误！");
-        } finally {
-            connDB.closeConn();
         }
     }
 
     @Override
     public void updateCategory(CommodityCategory category) throws Exception {
-        String sql = "update commodity_category set name=?,person_id=?,status=?,create_time=? where id=?";
+        /*String sql = "update commodity_category set name=?,person_id=?,status=?,create_time=? where id=?";
         PreparedStatement pstm = null;
         ConnDBUtil connDB = new ConnDBUtil();
         try {
@@ -66,19 +64,45 @@ public class CommodityCategoryDaoImpl implements CommodityCategoryDao {
             throw new Exception("更新错误！");
         } finally {
             connDB.closeConn();
+        }*/
+        String categoryId = category.getId();
+        if (StringUtil.isNull(categoryId)) {
+            throw new Exception("参数异常~");
         }
+        Integer count = 1;
+        String baseSql = "update commodity_category set name=?,person_id=?,status=?,create_time=? set id=" + categoryId + "";
+
+        if (!StringUtil.isNull(category.getName())) {
+            baseSql += ",person_id='" + category.getPersonId() + "'";
+            count++;
+        }
+
+        if (!StringUtil.isNull(category.getStatus())) {
+            baseSql += ",status ='" + category.getStatus() + "'";
+            count++;
+        }
+
+        if (!StringUtil.isNull(category.getCreateTime())) {
+            baseSql += ",create_time = '" + category.getCreateTime() + "'";
+            count++;
+        }
+
+        baseSql += "where id =" + categoryId + "";
+
+        Connection con = MySqlUtil.getCon();
+        PreparedStatement pre = con.prepareStatement(baseSql);
+        pre.executeUpdate();
     }
 
     @Override
     public CommodityCategory selectCategory(int id) throws Exception {
         CommodityCategory category = null;
         String sql = "select id,name,person_id,status,create_time where id=?";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
         try {
-            pstm = connDB.createPStmt(sql);
-            pstm.setString(1, category.getId());
-            ResultSet rs = connDB.pstmtQuery();
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pre = con.prepareStatement(sql);
+            pre.setString(1, category.getId());
+            ResultSet rs = pre.executeQuery();
             if (rs.next()) {
                 category = new CommodityCategory();
                 category.setId(rs.getString(1));
@@ -88,11 +112,9 @@ public class CommodityCategoryDaoImpl implements CommodityCategoryDao {
                 category.setCreateTime(rs.getString(5));
             }
             rs.close();
-            pstm.close();
+            pre.close();
         } catch (Exception e) {
-            throw new Exception("id查询错误！");
-        } finally {
-            connDB.closeConn();
+            throw new Exception("根据id查询错误！");
         }
         return category;
     }
@@ -102,12 +124,10 @@ public class CommodityCategoryDaoImpl implements CommodityCategoryDao {
         List ListAll = new ArrayList();
         String sql = "select * from commodity_category";
         PreparedStatement pstmt = null;
-        ConnDBUtil connDB = null;
-        connDB = new ConnDBUtil();
         try {
-            connDB.OpenConn();
-            pstmt = connDB.createPStmt(sql);
-            ResultSet rs = connDB.pstmtQuery();
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
             while (rs.next()) {
                 CommodityCategory comm = new CommodityCategory();
                 comm.setId(rs.getString(1));
@@ -122,8 +142,6 @@ public class CommodityCategoryDaoImpl implements CommodityCategoryDao {
         } catch (Exception e) {
             System.out.println(e);
             throw new Exception("查询全部错误！！！");
-        } finally {
-            connDB.closeConn();
         }
         return ListAll;
     }
--- src/cn/edu/xmut/lgrg/dao/impl/SysCommodityImpl.java
+++ src/cn/edu/xmut/lgrg/dao/impl/SysCommodityImpl.java
@@ -3,7 +3,9 @@ package cn.edu.xmut.lgrg.dao.impl;
 import cn.edu.xmut.lgrg.dao.SysCommodityDao;
 import cn.edu.xmut.lgrg.entity.SysCommodity;
 import cn.edu.xmut.lgrg.util.ConnDBUtil;
+import cn.edu.xmut.lgrg.util.MySqlUtil;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -14,11 +16,9 @@ public class SysCommodityImpl implements SysCommodityDao {
     @Override
     public void addComm(SysCommodity comm) throws Exception {
         String sql = "insert into sys_commodity(com_name,com_price,com_op,com_create_time,com_brand_id,com_category_id,detail_info,status,is_vip,com_img,com_stock) values(?,?,?,?,?,?,?,?,?,?,?)";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
-        connDB.OpenConn();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, comm.getComName());
             pstm.setDouble(2, comm.getComPrice());
             pstm.setDouble(3, comm.getComOp());
@@ -31,39 +31,32 @@ public class SysCommodityImpl implements SysCommodityDao {
             pstm.setString(10, comm.getComImg());
             pstm.setString(11, comm.getComStock());
             pstm.executeUpdate();
-            pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("新增数据错误！");
-        } finally {
-            connDB.closeConn();
         }
     }
 
     @Override
     public void deleteComm(SysCommodity comm) throws Exception {
         String sql = "delete from sys_commodity where com_id=?";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
-        connDB.OpenConn();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, comm.getComId());
             pstm.executeUpdate();
-            pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("删除数据错误！");
-        } finally {
-            connDB.closeConn();
         }
     }
 
     @Override
     public void updateComm(SysCommodity comm) throws Exception {
         String sql = "update sys_commodity set com_name=?,com_price=?,com_op=?,com_brand_id=?,com_category_id=?,detail_info=?,status=?,is_vip=?,com_stock=? where com_id=?";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, comm.getComName());
             pstm.setDouble(2, comm.getComPrice());
             pstm.setDouble(3, comm.getComOp());
@@ -77,9 +70,8 @@ public class SysCommodityImpl implements SysCommodityDao {
             pstm.executeUpdate();
             pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("更新数据错误！");
-        } finally {
-            connDB.closeConn();
         }
     }
 
@@ -87,12 +79,11 @@ public class SysCommodityImpl implements SysCommodityDao {
     public SysCommodity selectComm(int comId) throws Exception {
         SysCommodity comm = null;
         String sql = "Select com_id,com_name,com_price,com_create_time,com_brand_id,com_category_id,detail_info,is_vip from sys_commodity where com_id=?";
-        PreparedStatement pstm = null;
-        ConnDBUtil connDB = new ConnDBUtil();
         try {
-            pstm = connDB.createPStmt(sql);
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, comm.getComId());
-            ResultSet rs = connDB.pstmtQuery();
+            ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
                 comm = new SysCommodity();
                 comm.setComId(rs.getString(1));
@@ -107,9 +98,8 @@ public class SysCommodityImpl implements SysCommodityDao {
             rs.close();
             pstm.close();
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("id查询错误！");
-        } finally {
-            connDB.closeConn();
         }
         return comm;
     }
@@ -118,13 +108,10 @@ public class SysCommodityImpl implements SysCommodityDao {
     public List selectAllComm() throws Exception {
         List ListAll = new ArrayList();
         String sql = "select * from sys_commodity";
-        PreparedStatement pstmt = null;
-        ConnDBUtil connDB = null;
-        connDB = new ConnDBUtil();
         try {
-            connDB.OpenConn();
-            pstmt = connDB.createPStmt(sql);
-            ResultSet rs = connDB.pstmtQuery();
+            Connection con = MySqlUtil.getCon();
+            PreparedStatement pstm = con.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
             while (rs.next()) {
                 SysCommodity comm = new SysCommodity();
                 comm.setComId(rs.getString(1));
@@ -142,12 +129,9 @@ public class SysCommodityImpl implements SysCommodityDao {
                 ListAll.add(comm);
             }
             rs.close();
-            pstmt.close();
         } catch (Exception e) {
             System.out.println(e);
             throw new Exception("查询全部错误！！！");
-        } finally {
-            connDB.closeConn();
         }
         return ListAll;
     }
