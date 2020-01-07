6e67d517d46357eb4120be1878c80729568f7608
Mis-0 <1403658328@qq.com>
2020-01-07 08:43:01 +0800

    test

---
+++ src/cn/edu/xmut/lgrg/servlets/order/test.java
@@ -0,0 +1,12 @@
+package cn.edu.xmut.lgrg.servlets.order;
+
+/**
+ * @author azx
+ * @create 2020-01-07-8:41
+ */
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println(123);
+    }
+}
